7f1dd7c48b896b1c1240abbd272b3cfbc7ef99e1
Ulrich Norbisrath <devel@mail.ulno.net>
2020-10-05 09:52:54 -0300

    More spelling mistake fixes.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Author: ulno
 
 if __name__ == '__main__':
-    print('This is the Sytems Modeling extension for python.')
+    print('This is the Systems Modeling extension for python.')
     print('Check sysmdpy test_*.py for examples.')
     print('Check out https://github.com/ulno/sysmodpy for more.')
 
